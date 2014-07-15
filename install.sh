98bf23be5b7d290ceec10b07cfacf2662d9b266e
Gabriel Lett <glett@bionexo.com>
2014-07-15 08:34:27 -0300

    Added zshrc link

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@ curl -L http://install.ohmyz.sh | sh
 
 git clone https://github.com/gabriellett/.lett ~/.lett
 
+mv ~/.zshrc ~/.zshrc_bkp
+
 ln -s ~/.lett/.vim ~/.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
+ln -s ~/.lett/.zshrc ~/.zshrc
 
 cp ~/.lett/to-oh-my-zsh/themes/ys_powerline.zsh-theme ~/.oh-my-zsh/themes
 cp ~/.lett/to-oh-my-zsh/functions/git.zsh ~/.oh-my-zsh/functions
