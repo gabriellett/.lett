fe2bbf042d282b505655768b2472430fabac62b8
Gabriel Lett <glett@bionexo.com>
2015-02-19 08:18:36 -0200

    Adding vim plugins and zsh plugins clone

--- install.sh
+++ install.sh
@@ -11,4 +11,10 @@ ln -s ~/.lett/.zshrc ~/.zshrc
 cp ~/.lett/to-oh-my-zsh/themes/ys_powerline.zsh-theme ~/.oh-my-zsh/themes
 cp ~/.lett/to-oh-my-zsh/functions/git.zsh ~/.oh-my-zsh/functions
 
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+#vim stuff: 
+git clone https://github.com/mileszs/ack.vim ~/.lett/.vim/bundle/ack.vim
+git clone https://github.com/kien/ctrlp.vim.git ~/.lett/.vim/bundle/ctrlp.vim
+
 mkdir ~/.lett/.vim/tmp
