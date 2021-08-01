e9a680bd3dfaf9ea6c738edb5bbcac5361bf2e27
Gabriel Lett Viviani <gm.lett@gmail.com>
2021-08-01 15:34:44 +0200

    Improving uninstallation script

--- uninstall.sh
+++ uninstall.sh
@@ -3,3 +3,8 @@ unlink ~/.vimrc
 
 rm -rf ~/.oh-my-zsh
 rm -rf ~/.lett
+
+# We keep the backup as a backup lol
+cp -R ~/init_files_before_dotlett/* ~/
+
+echo "Not uninstalling brew packages, nvm or pip stuff as you might have installed some of those before, if you want, delete them manually, check the install script for a list of packages."
