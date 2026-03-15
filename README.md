2f65b47e140a5bf4cd4b256fd150c4d6f330a798
Gabriel Lett Viviani <gm.lett@gmail.com>
2026-03-15 12:34:42 +0100

    Installation script and isntructions

--- README.md
+++ README.md
@@ -2,7 +2,20 @@
 
 My dotfiles.
 
+## Requirements
+
+### On Arch
+
+* [hyprland](https://hypr.land/): Hyprland, Waybar etc are not part of the installation script, and installing this might break your setup right now (until I fix it)
+* zsh: This considers ZSH is your default sheel
+
+### On mac:
+
+* [brew](https://brew.sh/)
+
 ## Automatic installation:
+
+** Pleave review the script before running it, it might break your entire setup **
 
 ```
 curl -L https://raw.githubusercontent.com/gabriellett/.lett/master/install.sh | sh
@@ -10,58 +23,10 @@ curl -L https://raw.githubusercontent.com/gabriellett/.lett/master/install.sh | 
 
 ## Manual installation
 
-First you need to install [oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh)
-
 Then clone this repo in your home folder:
 	
 ```
 git clone https://github.com/gabriellett/.lett ~/.lett
 ```
 
-Link the .vim, .vimrc, .zshrc and .tmux.conf from .lett to your home
-
-To make powerline prettier copy `.lett/to-oh-my-zsh/themes/ys_powerline.zsh-theme` to `~/.oh-my-zsh/themes
-
-To make git directory info show on your shell, copy `.lett/to-oh-my-zsh/functions/git.zsh` to `~/.oh-my-zsh/functions`
-
-If you want imgcat to work, copy (using -R) `.lett/to-oh-my-zsh/plugins/*` to `~/.oh-my-zsh/plugins/`*
-
-If you want zshell to highlight suff on your command line, use: 
-
-
-```
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-```
-
-I use the following VIM plugins, clone the ones you want
-
-
-```
-# better ACK inside VIM
-git clone https://github.com/mileszs/ack.vim ~/.lett/.vim/bundle/ack.vim
-# CTRL-P for completion
-git clone https://github.com/kien/ctrlp.vim.git ~/.lett/.vim/bundle/ctrlp.vim
-# Easy motion for a more "visual" search
-git clone https://github.com/easymotion/vim-easymotion ~/.lett/.vim/bundle/vim-easymotion
-# Scala highlight
-git clone https://github.com/lenniboy/vim-scala ~/.lett/.vim/bundle/scala.vim
-# JS highlight
-git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
-# JSON highlight
-git clone https://github.com/elzr/vim-json.git ~/.vim/bundle/vim.json
-# rust highlight
-git clone --depth=1 https://github.com/rust-lang/rust.vim.git ~/.vim/bundle/rust.vim
-```
-
-If you want a pretty powerline on tmux, use:
-
-```
-pip3 install powerline-status
-pip3 install psutil
-```
-
-And finally create the temp VIM dir so vim stop complaining about it:
-
-```
-mkdir ~/.lett/.vim/tmp
-```
+And check install.sh on details 
--- install.sh
+++ install.sh
@@ -28,14 +28,16 @@ if [ "$OS" == "Darwin" ]; then
   brew install fzf ripgrep alacritty rectangle alfred tmux zoxide neovim starship font-jetbrains-mono-nerd-font
   brew install --cask spotify sip
 elif [ "$OS" == "Linux" ]; then
-  echo "This installer will consider hyprland is already installed (until I actually configure it to install it)"
-  mv ~/.config/elephant/desktopapplications.toml ~/.config/elephant/symbols.toml ~/.lett/.config/elephant/
-  mv ~/.config/hypr/base.conf ~/.config/hypr/binds.conf ~/.config/hypr/hyprland.conf ~/.config/hypr/menus.conf ~/.config/hypr/monitors.conf ~/.lett/.config/hypr/
-  mv ~/.config/waybar/config.jsonc ~/.config/waybar/scripts ~/.config/waybar/style.css ~/.lett/config/waybar/
+  # echo "This installer will consider hyprland is already installed (until I actually configure it to install it)"
+  echo "Run the following moves if you want to use the hypr config on this repo:"
+  echo "mv ~/.config/elephant/desktopapplications.toml ~/.config/elephant/symbols.toml ~/.lett/.config/elephant/"
+  echo "mv ~/.config/hypr/base.conf ~/.config/hypr/binds.conf ~/.config/hypr/hyprland.conf ~/.config/hypr/menus.conf ~/.config/hypr/monitors.conf ~/.lett/.config/hypr/"
+  echo "mv ~/.config/waybar/config.jsonc ~/.config/waybar/scripts ~/.config/waybar/style.css ~/.lett/config/waybar/"
   echo "Backup done!"
 
-  echo "Installing all tools from yay..."
+  echo "Installing all tools from AUR..."
   pacman -Sy btop cliphist colordiff fastfetch fzf htop kitty neovim obsidian starship tmux
+
   # pacman -Sy waybar
   # yay -Sy elephant elephant-calc elephant-clipboard elephant-desktopapplications \
   #   elephant-files elephant-menus elephant-providerlist elephant-runner elephant-symbols \
@@ -58,18 +60,20 @@ if [ "$OS" == "Darwin" ]; then
 elif [ "$OS" == "Linux" ]; then
   ln -s ~/.lett/.config/kitty ~/.config/kitty
 
-  ln -s ~/.lett/.config/elephant/desktopapplications.toml ~/.config/elephant/desktopapplications.toml
-  ln -s ~/.lett/.config/elephant/symbols.toml ~/.config/elephant/symbols.toml
+  echo "Run the following links according to your setup: \n\n"
 
-  ln -s ~/.lett/.config/hypr/base.conf ~/.config/hypr/base.conf
-  ln -s ~/.lett/.config/hypr/binds.conf ~/.config/hypr/binds.conf
-  ln -s ~/.lett/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
-  ln -s ~/.lett/.config/hypr/menus.conf ~/.config/hypr/menus.conf
-  ln -s ~/.lett/.config/hypr/monitors.conf ~/.config/hypr/monitors.conf
+  echo "ln -s ~/.lett/.config/elephant/desktopapplications.toml ~/.config/elephant/desktopapplications.toml"
+  echo "ln -s ~/.lett/.config/elephant/symbols.toml ~/.config/elephant/symbols.toml"
 
-  ln -s ~/.lett/.config/waybar/scripts ~/.config/waybar/scripts
-  ln -s ~/.lett/.config/waybar/config.jsonc ~/.config/waybar/config.jsonc
-  ln -s ~/.lett/.config/waybar/style.css ~/.config/waybar/style.css
+  echo "ln -s ~/.lett/.config/hypr/base.conf ~/.config/hypr/base.conf"
+  echo "ln -s ~/.lett/.config/hypr/binds.conf ~/.config/hypr/binds.conf"
+  echo "ln -s ~/.lett/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf"
+  echo "ln -s ~/.lett/.config/hypr/menus.conf ~/.config/hypr/menus.conf"
+  echo "ln -s ~/.lett/.config/hypr/monitors.conf ~/.config/hypr/monitors.conf"
+
+  echo "ln -s ~/.lett/.config/waybar/scripts ~/.config/waybar/scripts"
+  echo "ln -s ~/.lett/.config/waybar/config.jsonc ~/.config/waybar/config.jsonc"
+  echo "ln -s ~/.lett/.config/waybar/style.css ~/.config/waybar/style.css"
 fi
 echo "We are done! Open neovim to install the plugins."
 
